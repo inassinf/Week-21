74237f5a6c7391ea71763d1a36ceecdc63975d20
inassinf <inass.infantes@gmail.com>
2023-04-10 13:15:09 +0200

    reto terminado

--- Repaso2/algebraProject/point.ts
+++ Repaso2/algebraProject/point.ts
@@ -28,7 +28,27 @@ toString():string{
    
      return `Los puntos son: "(${this.x}, ${this.y})"`;
 }
+
+distanceTolOrigin():number{
+    let distancia=0;
+    distancia = Math.sqrt((this.x*this.x) + (this.y*this.y));
+
+    return distancia;
 }
+
+calculateDistance(anotherPoint:Point):number{
+    
+    let horizontal = this.x - anotherPoint.x;
+    let vertical = this.y - anotherPoint.y;
+    let distancia2;
+
+    distancia2 = Math.sqrt((horizontal*horizontal) + (vertical*vertical));
+    return distancia2;
+}
+
+}
+
+
 
 // let Point1 = new Point(2,5);
 // console.log(Point1.toString());
--- Repaso2/algebraProject/pointTest.ts
+++ Repaso2/algebraProject/pointTest.ts
@@ -1,9 +1,15 @@
 import { Point } from "./point";
 
-let punto1 = new Point(2,3);
+let punto1 = new Point(4,3);
+let punto2 = new Point(0,0);
 console.log(punto1.toString());
+console.log("La distancia al origen es: " + punto1.distanceTolOrigin());
 
-punto1.setX(4);
+
+
+punto1.setX(2);
 punto1.setY(5);
 
 console.log(punto1.toString());
+console.log("La distancia del " + punto1 + " al " + punto2+   "es: " + punto1.calculateDistance(punto2));
+
